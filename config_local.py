a3f2849ba5ad59bfb5219901b85a964a902be71a
Artur <39272116+YentanYN@users.noreply.github.com>
2020-04-04 01:21:30 +0300

    v1.0

---
+++ config_local.py
@@ -0,0 +1,14 @@
+# standart config without env (for local use and v1.0)
+
+vk_api_token = "29adba3ad91b5b631b008fb0cb0a7b4921a7cbf49b3ea5f9851b1d374fe93e5f8248f3e1d38de27e76529"
+vk_group_id = "193666020"
+vk_prefix1 = "[club193666020|club193666020]"
+vk_prefix2 = "[club193666020|БРАУЗЕР АМИГО]"
+button_text = "XnR_bot"
+message1 = "#msg1_text"
+message2 = "#msg2_text"
+message3 = "#msg3_text"
+
+# don't touch this
+Debug = True
+delay_send = 0.1
---
+++ main.py
@@ -0,0 +1,95 @@
+print("Starting XnR_bot!")
+import vk_api.vk_api
+from vk_api.bot_longpoll import VkBotEventType
+from vk_api.bot_longpoll import VkBotLongPoll
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.utils import get_random_id
+import sys, os, random
+import asyncio, kb_gen
+print("Libraries is OK")
+# WARNING! if you start bot on heroku isLocal must be false
+isLocal = True
+# WARNING!
+
+# for choice local cfg or cfg
+if isLocal:
+    from config_local import vk_api_token, vk_group_id, vk_prefix1, vk_prefix2, button_text, message1, message2, message3, Debug, delay_send
+    print("Local config selected!")
+else:
+    from config import vk_api_token, vk_group_id, vk_prefix1, vk_prefix2, button_text, message1, message2, message3, Debug, delay_send
+    print("Env config selected!")
+
+class BotCore:
+    def __init__(self, api_token, group_id, server_name: str = "Empty"):
+        self.server_name = server_name
+        self.vk = vk_api.VkApi(token=api_token)
+        self.long_poll = VkBotLongPoll(self.vk, group_id)
+        self.vk_api = self.vk.get_api()
+        print("BotCore class initilaized!")
+
+    #func for send message with keyboard
+    async def send_msg_kb(self, send_id, message, kb):
+        self.vk_api.messages.send(peer_id=send_id, message=message, keyboard=kb, random_id=get_random_id())
+        return 1
+
+    def adjust_message_text(self, msg):
+        message_text = msg.encode()
+        if len(message_text) < 4096:
+            message_text = message_text * int(4096 / len(message_text))
+            return message_text.decode()
+
+    #func for spam
+    async def spam(self, send_id, msg1, msg2, msg3, delay_send, button_txt):
+        print("Starting raid conversation!")
+        while True:
+            try:
+                kbs_s = kb_gen.gen(button_text)
+                await self.send_msg_kb(send_id, random.choice([msg1, msg2, msg3]), kbs_s)
+                await self.send_msg_kb(send_id, random.choice([msg1, msg2, msg3]), kbs_s)
+                await self.send_msg_kb(send_id, random.choice([msg1, msg2, msg3]), kbs_s)
+                await asyncio.sleep(delay_send)
+            except vk_api.exceptions.ApiError as f:
+                f2 = str(f)
+                if f2 == "[9] Flood control: too much messages sent to user":
+                    print("Flood control detected!")
+                else:
+                    print(f'Stopping raid this conversation. reason:{f}')
+                    break
+
+    #main cycle
+    def start(self):
+        print("XnR_bot started!")
+        print("Starting listeing long poll!")
+        for event in self.long_poll.listen():
+                if Debug:
+                    print(event)
+                if event.type == VkBotEventType.MESSAGE_NEW:
+                    last_id = event.object.peer_id
+                    print("New message received from "+str(last_id))
+                    # make button for activation raid
+                    if event.object.text == '' or event.object.text == vk_prefix1+' rqb' or event.object.text == vk_prefix2+' rqb':
+                        if event.object.text == '':
+                            print("Joined to conversation!")
+                            print("Sending keyboard")
+                        else:
+                            print("Request to send keyboard received!")
+                            print("Sending keyboard")
+                        kb = VkKeyboard(one_time=True)
+                        kb.add_button("pwn", VkKeyboardColor.NEGATIVE)
+                        kb_main = kb.get_keyboard()
+                        ioloop = asyncio.get_event_loop()
+                        wait_tasks = asyncio.wait([self.send_msg_kb(last_id, 'XnR_bot. Powered by Heroku.', kb_main)])
+                        ioloop.run_until_complete(wait_tasks)
+                        print("Keyboard sent!")
+                    # starts raid to the conversation
+                    if event.object.text == vk_prefix2+' pwn' or event.object.text == vk_prefix1+' pwn':
+                        print("Message received for start raid!")
+                        ioloop = asyncio.get_event_loop()
+                        wait_tasks = asyncio.wait([self.spam(last_id, self.adjust_message_text(message1), self.adjust_message_text(message2)
+                        , self.adjust_message_text(message3), delay_send, button_text)])
+                        ioloop.run_until_complete(wait_tasks)
+
+
+xnrBot = BotCore(vk_api_token, vk_group_id, "XnR_bot")
+while True:
+    xnrBot.start()
