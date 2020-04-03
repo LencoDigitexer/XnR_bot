bb2d7e56d5579aae7cccb4b4a0ef167df7074840
Artur <39272116+YentanYN@users.noreply.github.com>
2020-04-04 01:29:50 +0300

    Update config_local.py

--- config_local.py
+++ config_local.py
@@ -1,9 +1,9 @@
 # standart config without env (for local use and v1.0)
 
-vk_api_token = "29adba3ad91b5b631b008fb0cb0a7b4921a7cbf49b3ea5f9851b1d374fe93e5f8248f3e1d38de27e76529"
-vk_group_id = "193666020"
-vk_prefix1 = "[club193666020|club193666020]"
-vk_prefix2 = "[club193666020|БРАУЗЕР АМИГО]"
+vk_api_token = ""
+vk_group_id = ""
+vk_prefix1 = ""
+vk_prefix2 = ""
 button_text = "XnR_bot"
 message1 = "#msg1_text"
 message2 = "#msg2_text"
