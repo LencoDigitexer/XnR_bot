bd1341a694d66aea746210402f085b98a8608317
Artur <39272116+arturyudin@users.noreply.github.com>
2020-07-14 02:17:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config for raid bot with env (for heroku and v1.0)
 import os
 
-vk_api_token = os.environ.get('vk_api_tokenn')
+vk_api_token = os.environ.get('vk_api_token')
 vk_group_id = os.environ.get('vk_group_id')
 vk_prefix1 = os.environ.get('vk_prefix1')
 vk_prefix2 = os.environ.get('vk_prefix2')
