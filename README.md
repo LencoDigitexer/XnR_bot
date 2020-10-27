396d678984072fdf664729652fb6da05ad5c98ce
Artur <39272116+arturyudin@users.noreply.github.com>
2020-10-27 23:27:02 +0300

    XyeCoc

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1 align="center">XnR_bot</h1>
+<p align="center">
+    <img alt="Made with Python" src="https://img.shields.io/badge/Made%20with-Python-%23FFD242?logo=python&logoColor=white"></img>
+</p>
+Рейд бот для ВКонтакте
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/YentanYN/XnR_bot)
---
+++ config.py
@@ -0,0 +1,16 @@
+# config for raid bot with env (for heroku and v1.0)
+import os
+
+vk_api_token = os.environ.get('vk_api_token')
+vk_group_id = os.environ.get('vk_group_id')
+vk_prefix1 = os.environ.get('vk_prefix1')
+vk_prefix2 = os.environ.get('vk_prefix2')
+button_text = os.environ.get('button_text')
+message1 = os.environ.get('message1')
+message2 = os.environ.get('message2')
+message3 = os.environ.get('message3')
+start_button_text = os.environ.get('start_button_text')
+
+#don't touch this
+Debug = False
+delay_send = 0.1
